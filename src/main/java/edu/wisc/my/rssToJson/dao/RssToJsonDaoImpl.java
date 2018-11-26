537f46f45b0526292877a076487199e0285a669c
Doug Reed <doug.reed@wisc.edu>
2018-11-26 08:42:57 -0600

    interim commit

--- src/main/java/edu/wisc/my/rssToJson/dao/RssToJsonDaoImpl.java
+++ src/main/java/edu/wisc/my/rssToJson/dao/RssToJsonDaoImpl.java
@@ -2,15 +2,22 @@ package edu.wisc.my.rssToJson.dao;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import org.apache.http.HttpHeaders;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONObject;
 import org.json.XML;
-import javax.json.JsonReader;  
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils; 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,25 +42,51 @@ public class RssToJsonDaoImpl implements RssToJsonDao{
     @Autowired
     void setEnv(Environment env) {
       this.env = env;
+    }   
+    private String httpResponseAsString(String feedName) throws IOException {
+        logger.error("HTTP Response method " + feedName);
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            String url = getEndpointURL(feedName);
+                         logger.error("ONE " + url);
+            HttpGet httpget = new HttpGet(url);
+            logger.error(httpget.toString());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                     logger.error("TWO " + response.toString());        
+                    int status = response.getStatusLine().getStatusCode();
+                    logger.debug(status + " response code ");
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+            };
+            logger.error("ONE POINT FIVE " + httpclient.toString());
+        
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            logger.error("ONE POINT SIX " + responseBody);
+            return responseBody;
+        } finally {
+            httpclient.close();
+        }
     }
+
 
     @Override
     public JSONObject getXMLFeed(String feedEndpoint) {
        String endpointURL = getEndpointURL(feedEndpoint);
        JSONObject jsonObject = null;
        try {
-           HttpClient client = HttpClientBuilder.create().build();
-           HttpGet request = new HttpGet(endpointURL);
-           request.setHeader(HttpHeaders.USER_AGENT, "rss-to-json service");
-           HttpResponse response = client.execute(request);
-           InputStreamReader isr = new InputStreamReader(response.getEntity().getContent());
-           StringBuffer stringBuffer = new StringBuffer();
-           int i;
-           while ((i = isr.read()) != -1) {
-               stringBuffer.append((char) i);
-           }
-           String xmlString = stringBuffer.toString();
-           jsonObject = XML.toJSONObject(xmlString);
+         String xmlString = httpResponseAsString(endpointURL);
+         jsonObject = XML.toJSONObject(xmlString);
        } catch (Exception e) {
            logger.warn(e.getMessage());
            return null;
@@ -62,7 +95,9 @@ public class RssToJsonDaoImpl implements RssToJsonDao{
    }
    
    private String getEndpointURL(String feed) {
+       logger.error("GETTING THE ENDPOINT FOR " + feed);
        String endpointURL = env.getProperty(feed);
+       logger.error(endpointURL);
        if (endpointURL == null) {
            logger.warn("No corresponding feed url for requested endpoint {}", feed);
            return null;
@@ -73,31 +108,20 @@ public class RssToJsonDaoImpl implements RssToJsonDao{
     @Override
     @Cacheable(cacheNames="feeds", sync=true)
     public SyndFeed getRssFeed(String feedEndpoint) {
-        logger.info("Fetching feed for {} ", feedEndpoint);
-        //see if property file has corresponding url for requested endpoint
-        String endpointURL = env.getProperty(feedEndpoint);
-        if (endpointURL == null){
-          logger.warn("No corresponding feed url for requested endpoint {}",
-                  feedEndpoint);
-          return null;
-        }
-        SyndFeed feed = null;
-        try{
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpGet request = new HttpGet(endpointURL);
-            request.setHeader(HttpHeaders.USER_AGENT, "rss-to-json service");
-            request.setHeader(HttpHeaders.CONTENT_ENCODING, "UTF-8");
-            HttpResponse response = client.execute(request);
-            SyndFeedInput input = new SyndFeedInput();
-            feed = input.build(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-            feed.setFeedType("UTF-8");
-            logger.debug("CONTENT OF FEED " + endpointURL);
-            logger.debug(feed.toString());
-
-        }catch(Exception ex){
-            logger.error("Error while fetching xml from {}", endpointURL, ex);
-        }
+      try{  
+        logger.error("INTO THE TRY");  
+        String result = httpResponseAsString(feedEndpoint);
+        SyndFeedInput input = new SyndFeedInput();
+        InputStream stream = new ByteArrayInputStream(result.getBytes("UTF-8"));
+        SyndFeed feed = input.build(new InputStreamReader(stream, "UTF-8"));
+        logger.debug("CONTENT OF FEED " + feedEndpoint);
+        logger.debug(feed.toString());
         return feed;
+      } catch (Exception e) {
+          logger.warn("Could not get feed " + feedEndpoint + " " + e.getMessage());
+      } 
+
+      return null;
     }
 
 }
